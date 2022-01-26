d551be0c54b577368d10b0b668765d666fd4b0e6
ADHIRAJ01 <www.praffulpratap@gmail.com>
2022-01-26 11:15:27 +0530

    FIRST UPLOAD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ViewpageComponent } from './viewpage/viewpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ViewpageComponent
   ],
   imports: [
     BrowserModule,
